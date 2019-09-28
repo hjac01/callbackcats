33b0bca94e99cefc38add58e2348ab301d9b274f
Colin D <colinv120@gmail.com>
2019-09-28 11:21:50 -0600

    scheme written and table defined in sqlize

--- models/example.js
+++ models/example.js
@@ -1,7 +1,15 @@
-module.exports = function(sequelize, DataTypes) {
-  var Example = sequelize.define("Example", {
-    text: DataTypes.STRING,
-    description: DataTypes.TEXT
+module.exports = function (sequelize, DataTypes) {
+  var Games = sequelize.define("Games", {
+    year: DataTypes.INTEGER,
+    round: DataTypes.INTEGER,
+    region_number: DataTypes.INTEGER,
+    region_name: DataTypes.STRING,
+    seed: DataTypes.INTEGER,
+    score: DataTypes.INTEGER,
+    team: DataTypes.STRING,
+    team_2: DataTypes.STRING,
+    score_2: DataTypes.INTEGER,
+    seed_2: DataTypes.INTEGER
   });
-  return Example;
+  return Games;
 };
