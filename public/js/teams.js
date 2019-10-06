c2c2e5cb44d4ef6890d7f7a91f494c91de2b4d76
David Cardenas <davidcardenas1993@yahoo.com>
2019-10-05 22:35:11 -0600

    created roster handlebars file for roster routing, created new api route, added new logos due to conflicts with team information.

--- public/js/teams.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-
-    $('.icon-container').on('click', '.mr-3', function(event) {
-        let skewl = $(this).attr('data');
-        console.log(skewl);
-    });
-});
