01a1a8892c545932823c4a1105ea50d264a3463d
Edwin Marquez <marquez.diaz08@gmail.com>
2019-10-08 00:17:56 -0600

    edwinroster

--- routes/api-dbRoutes.js
+++ routes/api-dbRoutes.js
@@ -1,48 +1,19 @@
 // schedule routes to view all of the routes 
 var db = require ("../models");
+var Rosters = require("../models/roster");
 
 module.exports = function(app) {
     //should get all the databases and display ahve the right setting to display data
     app.get("/api/schedule", function(req, res) {
-        //db.Schedule.findOne({ where: { id: req.params.id}}),
-        //db.Roster.findOne({ where: {id: req.params.id}}),
         db.Schedule.findAll({}).then(function(dbSchedule){
             res.json(dbSchedule);
         });
     });
 
-    app.get("/api/rosters", function(req, res) {
-      db.Roster.findAll({}).then(function(dbRoster){
-        res.json(dbRoster)
-      });
+    app.get("/api/rosters/:id", function(req, res) {
+      db.Rosters.findAll({where: { school: req.params.id}})
+      .then(function (dbRosters){
+          res.json(dbRosters)
+      })
     });
-
-    //comments data base to do a get post.
-    app.post("/api/comments", function(req, res) {
-        db.Create.create({
-          title: req.body.title,
-          body: req.body.body,
-          category: req.body.category
-        }).then(function(dbPost) {
-            res.json(dbPost);
-          });
-
-    app.get("*", function(req, res) {
-        res.render("404");
-      });
-
-    });
-    
-    
-
-    // app.get("/api/schedule", function(req, res) {
-    //     db.Schedule.findAll({}).then(function(dbSchedule) {
-    //     res.json(dbSchedule)
-    //     });
-    // });
-    // app.get("/api/schedule/:id", function(req, res) {
-    //     db.Schedule.findOne({where: {id: req.params.id}}).then(function(dbSchedule) {
-    //         res.json(dbSchedule)
-    //     });
-    // });
 };
--- routes/path.js
+++ routes/path.js
@@ -3,14 +3,17 @@ var path = require("path");
 module.exports = function(app) {
     //home
     app.get("/", function(req, res) {
-        res.render("index")
+        res.render("landing")
     });
     //teams 
     app.get("/team", function(req, res) {
         res.render("team")
+    });
+    app.get("api/schedule", function(req, res) {
+        res.render("schedule")
     })
     // teams s,r,
-    app.get("/example", function(req, res) {
-        res.render("example")
-    });
+       app.get("/api/rosters/:id", function(req, res) {
+         res.render("rosters")
+     });
 };
